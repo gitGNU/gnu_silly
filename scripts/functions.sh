f29381a7fb90d3c9abc75ebe3c99dac748da3b02
andyman <andyman@localhost.localdomain>
2016-03-12 13:25:48 -0800

    fixed strap func, dunno how

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -6,13 +6,10 @@ oneArg () {
     }
 
 devicePresent () {
-    [ -e $TARGET_DEVICE ] || {echo "No such device node as $TARGET_DEVICE" ; return 1 ; }
+    [ -e $1 ] || { echo "No such device node as $TARGET_DEVICE" ; return 1 ; }
     return 0
     }
 
-getDeviceNum () {
-    [ handleArgs -eq 1 ] || {echo "This command takes exactly one argument." ; return 1 ; }
-    }
     
 strapIt () {
     # Usage: `strapping [pxe|phy] $ARCH MOUNTPOINT`
@@ -23,7 +20,7 @@ strapIt () {
     # with 'phy' as default.
     # Example `strapping amd64 /mnt/debian-mountpoint/`
     local ARCH=$1
-    [ grep $ARCH ./arches.txt ] || return 1
+    grep $ARCH ../scripts/arches.txt || return 1
     local MOUNTPOINT=$2
     $3{=:"phy"}
     local BOOT_TYPE=$3
